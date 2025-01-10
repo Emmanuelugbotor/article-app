5a727ac44f4e8dc553e579b358bcce7471dd74aa
Emmanuel Ugbotor <emmanuel.ugbotor@mtn.com>
2025-01-10 19:05:50 +0100

    Added ErrorBoundary for application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import ArticleContainer from "./containers/ArticleContainer/ArticleContainer";
 
 function App() {
   return (
-    <>
+    <ErrorBoundary>
       <ArticleContainer />
-    </>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import React, { Component, ReactNode } from "react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | null;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    console.error("Error caught by ErrorBoundary:", errorInfo, error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ textAlign: "center", padding: "20px" }}>
+          <h1>Oopss!.</h1>
+          <p>{this.state.error?.message}</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
