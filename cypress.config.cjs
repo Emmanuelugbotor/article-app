34f5c0e30e6880c2eca3b8be20072c5153eb8091
Emmanuel Ugbotor <emmanuel.ugbotor@mtn.com>
2025-01-10 19:00:39 +0100

    added configuration for cypress test

---
+++ cypress.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {},
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    baseUrl: "http://localhost:5173",
+  },
+};
--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,17 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
-    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
   },
 };
 
+export default config;
