e4396669eaae3d027a7db9ee14edd2f252324cf6
Emmanuel Ugbotor <emmanuel.ugbotor@mtn.com>
2025-01-11 12:06:11 +0100

    Creating SonarCloud scanner configurations

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { IArticleCardProps } from "../../models/Article";
 
 const ArticleCard: React.FC<IArticleCardProps> = ({ article, onClick }) => (
-  <div className="article-card fill" data-testid="article-card" role="button" onClick={onClick}>
-    <p>{article.title}</p>
+  <div className="article-card fill" data-testid="article-card">
+    <p>
+      <button onClick={onClick}>{article.title}</button>
+    </p>
   </div>
 );
 
